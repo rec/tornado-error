41ec7e4261244c99f10449014297f24db7269444
Tom Swirly <tom@swirly.com>
2012-12-14 00:09:04 -0500

    Now we're getting the page image

--- helloworld.py
+++ helloworld.py
@@ -11,15 +11,21 @@ from tornado.options import define, options
 
 define('port', default=8888, help='run on the given port', type=int)
 
+MAIN_PAGE = """<html><head></head><body>This is a page that exists!<p>
+Click <a href="page-doesnt-exist.html">here</a> for a 404 page.</a>
+"""
+
 class MainHandler(tornado.web.RequestHandler):
   def get(self):
-    self.write('Hello, keywords!')
+    self.write(MAIN_PAGE)
 
 def main():
   tornado.options.parse_command_line()
   static_path = os.path.join(os.path.dirname(__file__), 'static')
   handlers = [(r'/', MainHandler)]
-  application = tornado.web.Application(handlers, static_path=static_path)
+  application = tornado.web.Application(handlers,
+                                        static_path=static_path
+                                        )
   http_server = tornado.httpserver.HTTPServer(application)
   http_server.listen(options.port)
   tornado.web.ErrorHandler = ErrorHandler.ErrorHandler
